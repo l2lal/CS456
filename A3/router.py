9aedca52b7f7e7df5c4976534791780dffcdfb1e
l2lal <l2lal@uwaterloo.ca>
2018-11-29 19:40:56 -0500

    Add files via upload

---
+++ A3/router.py
@@ -0,0 +1,144 @@
+import sys
+import socket
+
+NBR_ROUTER = 5
+NUM_INPUTS = 4
+
+def Check_Inputs(args):
+	
+
+	if len(args) != NUM_INPUTS:
+		print "Improper number of arguments"
+		exit(1)
+	
+	try:
+		req_code = int(args[0])
+
+	except:
+		print "Improper formatting of argument", args
+		exit(1)
+
+	try:
+	    socket.inet_aton(args[1])
+	    # legal
+	except socket.error:
+	    # Not legal
+
+	try:
+		req_code = int(args[2])
+
+	except:
+		print "Improper formatting of argument", args
+		exit(1)
+
+	try:
+		req_code = int(args[3])
+
+	except:
+		print "Improper formatting of argument", args
+		exit(1)
+
+	return 
+
+class pkt_HELLO(object):
+	def __init__(self):
+		self.router_id = None
+		self.link_id = None
+
+class pkt_LSPDU(object):
+	def __init__(self):
+		self.sender
+		self.router_id = None
+		self.link_id = None
+		self.cost = None
+		self.via = None
+
+class pkt_INIT(object):
+	def __init__(self, id):
+		self.router_id = id
+
+class link_cost(object):
+	def __init__(self):
+		self.link = None
+		self.cost = None
+
+class circuit_DB(object):
+	def __init__(self):
+		self.nbr_link = None
+		self.linkcost = [ link_cost() for i in range(NBR_ROUTER)]
+
+class router(object):
+	def __init(self):
+
+
+#Function Create_UDP - creates server UDP socket
+#Parameters: 0
+#Return:
+#	$1: <serverUDPSocket> handle to UDP Socket
+#	$2: <neg_port> UDP port
+def Create_UDP(port):
+	#Create server UDP Socket and listen to the negotiate port
+	routerUDPSocket = socket(AF_INET, SOCK_DGRAM)
+	routerUDPSocket.bind(('', port))
+	routerUDPHost, routerPort = routerUDPSocket.getsockname()
+
+	return (routerUDPSocket, routerPort)
+
+def Send_Init(routerUDPSocket, packet, nse_host, nse_port):
+	#Set timeout value to ensure no system hanging; Creating timeout value of 1 minute
+	#timeout = time.time() + 60
+
+	#Listen for the request code
+	print "Sending INIT Packet..." 
+	routerUDPSocket.sendto(str(packet).encode(), (nse_host, nse_port))
+
+def Wait_Init()
+	while True: 
+		receive_pkt nseAddress = serverUDPSocket.recvfrom(1024)
+
+		if receive_pkt.decode().nbr_link <= str(NBR_ROUTER): # code isn't correct, keep listening
+			print "Invalid request code"
+			if time.time() > timeout:
+				print "Timed out waiting for request code"
+				got_msg = False
+				exit(1)
+
+			else: #code is correct, let's do stuff
+				print "Something received"
+				got_msg = True
+				break
+
+	#serverUDPSocket.close()
+	print "linkcosts.. = ", str(receive_pkt.decode().linkcost)
+
+	return got_msg
+
+def main():
+	#validate inputs
+	Check_Inputs(sys.argv[1:])
+	
+	#assign inputs
+	router_id = sys.argv[1]
+	nse_host = sys.argv[2]
+	nse_port = sys.argv[3]
+	router_port = sys.argv[4]
+
+	timeout = time.time() + 60 * 5
+
+	#Create UDP Socket
+	routerUDPSocket, routerPort = Create_UDP(router_port)
+
+	#send 5 INIT packets to emulate 5 routers
+	for x in range(5):
+		print "sending packet num = ", x 
+		init_pkt = pkt_INIT(router_id+x)
+		Send_Init(routerUDPSocket, init_pkt, nse_host, nse_port)
+
+	#wait for a return packet
+	Wait_Init()
+
+	#while True:
+
+	#pythontops.com/ python socket network programming
+
+main()
