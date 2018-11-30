e03f044533134ca3e9718c5072aaaad52f341561
l2lal <l2lal@uwaterloo.ca>
2018-11-30 03:20:34 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -116,7 +116,7 @@ def Wait_Init(routerUDPSocket):
 		# 		break
 
 	#serverUDPSocket.close()
-	origsize = struct.unpack('%sI' % len(receive_pkt), receive_pkt)
+	origsize = struct.unpack('%sI' % len(receive_pkt), *receive_pkt)
 	print "linkcosts.. = ", origsize
 	return False
 
