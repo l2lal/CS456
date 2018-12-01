4fad536017946900217d4e2de58b6123c63dd6fe
l2lal <l2lal@uwaterloo.ca>
2018-12-01 11:19:10 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -207,7 +207,7 @@ def Update_and_Foward_LSPDU(routerUDPSocket, router, nse_host, nse_port):
 	updated = False
 	count = 0
 
-	while Check_Full(router) == False
+	while(!Check_Full(router))
 		receive_pkt, nseAddress = routerUDPSocket.recvfrom(1024)
 
 		if(receive_pkt):
