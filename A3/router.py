83d8e7bcb9be306da5b7c17e48f02e13c7667c7f
l2lal <l2lal@uwaterloo.ca>
2018-12-02 01:30:28 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -4,6 +4,7 @@ from socket import *
 import time
 import struct
 from collections import defaultdict
+from collections import deque, namedtuple
 
 NBR_ROUTER = 5
 NUM_INPUTS = 4
@@ -82,6 +83,7 @@ class Router(object):
 		self.neighbor_list = []
 		self.forwarded = []
 		self.rib = defaultdict(list)
+		self.rib[0] = ['Local', 0] #router, cost
 
 
 
@@ -180,8 +182,6 @@ def Send_All_LSPDU(routerUDPSocket, router, nse_host, nse_port):
 					packet = pkt_LSPDU(sender, router_id, link, cost, via)
 					Send_LSPDU(routerUDPSocket, router, nse_host, nse_port, packet)
 					print 'Sending a LS_PDU packet...'
-					if [packet.router_id,packet.link_id] not in router.testlist:
-						router.testlist.append([packet.router_id,packet.link_id])
 				else:
 					#router has no entries in this index of its LSDB
 					continue
@@ -249,6 +249,95 @@ def Update_and_Foward_LSPDU(routerUDPSocket, router, nse_host, nse_port):
 	print "Fully updated our LSPDU"
 
 
+
+# we'll use infinity as a default distance to nodes.
+inf = float('inf')
+Edge = namedtuple('Edge', 'start, end, cost')
+
+def make_edge(start, end, cost=1):
+  return Edge(start, end, cost)
+
+
+class Graph:
+    def __init__(self, edges):
+        # let's check that the data is right
+        wrong_edges = [i for i in edges if len(i) not in [2, 3]]
+        if wrong_edges:
+            raise ValueError('Wrong edges data: {}'.format(wrong_edges))
+
+        self.edges = [make_edge(*edge) for edge in edges]
+
+    @property
+    def vertices(self):
+        return set(
+            sum(
+                ([edge.start, edge.end] for edge in self.edges), []
+            )
+        )
+
+    def get_node_pairs(self, n1, n2, both_ends=True):
+        if both_ends:
+            node_pairs = [[n1, n2], [n2, n1]]
+        else:
+            node_pairs = [[n1, n2]]
+        return node_pairs
+
+    def remove_edge(self, n1, n2, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        edges = self.edges[:]
+        for edge in edges:
+            if [edge.start, edge.end] in node_pairs:
+                self.edges.remove(edge)
+
+    def add_edge(self, n1, n2, cost=1, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        for edge in self.edges:
+            if [edge.start, edge.end] in node_pairs:
+                return ValueError('Edge {} {} already exists'.format(n1, n2))
+
+        self.edges.append(Edge(start=n1, end=n2, cost=cost))
+        if both_ends:
+            self.edges.append(Edge(start=n2, end=n1, cost=cost))
+
+    @property
+    def neighbours(self):
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for edge in self.edges:
+            neighbours[edge.start].add((edge.end, edge.cost))
+
+        return neighbours
+
+    def dijkstra(self, source, dest):
+        assert source in self.vertices, 'Such source node doesn\'t exist'
+        distances = {vertex: inf for vertex in self.vertices}
+        previous_vertices = {
+            vertex: None for vertex in self.vertices
+        }
+        distances[source] = 0
+        vertices = self.vertices.copy()
+
+        while vertices:
+            current_vertex = min(
+                vertices, key=lambda vertex: distances[vertex])
+            vertices.remove(current_vertex)
+            if distances[current_vertex] == inf:
+                break
+            for neighbour, cost in self.neighbours[current_vertex]:
+                alternative_route = distances[current_vertex] + cost
+                if alternative_route < distances[neighbour]:
+                    distances[neighbour] = alternative_route
+                    previous_vertices[neighbour] = current_vertex
+
+        path, current_vertex = deque(), dest
+        while previous_vertices[current_vertex] is not None:
+            path.appendleft(current_vertex)
+            current_vertex = previous_vertices[current_vertex]
+        if path:
+            path.appendleft(current_vertex)
+        return path
+
+
+
 def main():
 	#validate inputs
 	Check_Inputs(sys.argv[1:])
@@ -289,6 +378,8 @@ def main():
 	#Update LSPDUs 
 	Update_and_Foward_LSPDU(routerUDPSocket,router,nse_host,nse_port)
 	print router.LSDB
+
+
 	#while True:
 
 	#pythontops.com/ python socket network programming
