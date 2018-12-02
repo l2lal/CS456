6415cfe38cc5849224e0296422ae18e782f2b003
l2lal <l2lal@uwaterloo.ca>
2018-12-02 02:57:40 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -337,7 +337,7 @@ class Graph:
             current_vertex = previous_vertices[current_vertex]
         if path:
             path.appendleft(current_vertex)
-        return path
+        return [path, 
 
 def Update_Graph(router):
 	for i in range(len(router.LSDB)):
@@ -352,7 +352,9 @@ def Update_Graph(router):
 					if ([a,b,cost] not in router.edges[0]): #and ([b,a,cost] not in router.edges[0]):
 						router.edges[0].append([a, b, cost])
 
-#def Build_RIB(router): 
+# def Build_RIB(router): 
+# 	total_cost = 0
+# 	for i in range(len(router.))
 
 
 def main():
@@ -400,7 +402,9 @@ def main():
 	#print router.edges[0]
 	router.graph = Graph(router.edges[0])
 
-	print(router.graph.dijkstra(3, 1))
+	path = (router.graph.dijkstra(3, 1))
+	print (path)
+	print (path[0])
 
 
 
