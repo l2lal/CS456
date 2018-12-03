f056a97eaf52da9d0f466d182537541603427fb6
l2lal <l2lal@uwaterloo.ca>
2018-12-03 17:52:37 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -388,16 +388,19 @@ def Build_RIB(router):
 	r_a = router.id
 	source_in_edges = False
 	for rout in range(NBR_ROUTER):
+		dest_in_edges = False
 		if(rout != router.id-1):
 			r_b = rout + 1
 			# Check if source node, r_a exists
 			print(router.LSDB)
 			print(router.edges[0])
 			for check in range(len(router.edges[0])):
-				if r_a == ((router.edges[0])[check])[0]:
+				if(r_a == ((router.edges[0])[check])[0]):
 					source_in_edges = True
+				if(r_b == ((router.edges[0])[check])[1]):
+					dest_in_edges = True
 					print ("source is in edges!")
-			if (len(router.LSDB[rout]) > 0) and (len(router.LSDB[r_a-1]) > 0) and (len(router.edges[0]) > 0) and (source_in_edges):
+			if (len(router.LSDB[rout]) > 0) and (len(router.LSDB[r_a-1]) > 0) and (len(router.edges[0]) > 0) and (source_in_edges) and (dest_in_edges):
 				print("Dijkstra: passing in ", r_a, " and ", r_b)
 				path = (router.graph.dijkstra(r_a, r_b))
 				total_cost = 0 
