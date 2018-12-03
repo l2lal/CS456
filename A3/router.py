003f6c64c2082c7db8c0333aa661fb0e06ee33e0
l2lal <l2lal@uwaterloo.ca>
2018-12-03 17:16:37 -0500

    Add files via upload

--- A3/router.py
+++ A3/router.py
@@ -381,7 +381,7 @@ def Build_RIB(router):
 		if(rout != router.id-1):
 			r_b = rout + 1
 			# Check if source node, r_a exists
-			print "Dijkstra: passing in " r_a " and " r_b
+			print("Dijkstra: passing in ", r_a, " and ", r_b)
 			path = (router.graph.dijkstra(r_a, r_b))
 			total_cost = 0 
 			for i in range(len(path)-1):
